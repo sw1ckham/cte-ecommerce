cf850d5e583962c9baded4a3678363a0b66a34bd
sw1ckham <sphwick@gmail.com>
2020-05-01 14:47:17 +0000

    Added email host info for password reset

--- CTEecommerce/settings.py
+++ CTEecommerce/settings.py
@@ -140,3 +140,9 @@ STATICFILES_DIRS = [
 ]
 
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
+EMAIL_PORT = 587
