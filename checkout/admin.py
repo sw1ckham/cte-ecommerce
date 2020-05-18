34deada0a0a0e3be2d893bf74e025aa2719dfd66
Sophie Wickham <sphwick@gmail.com>
2020-05-18 13:13:35 +0000

    Created checkout link

---
+++ checkout/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import OrderLineItem, Order
+
+# Register your models here.
+
+class OrderLineAdminInline(admin.TabularInline):
+    model = OrderLineItem
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderLineAdminInline, )
+
+admin.site.register(Order, OrderAdmin)
