662fa5c797a33d0b9146c9346e6e808e5acf2caf
Sophie Wickham <sphwick@gmail.com>
2020-05-18 14:24:28 +0000

    checking for error

--- CTEecommerce/settings.py
+++ CTEecommerce/settings.py
@@ -149,7 +149,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
-STRIPE_SECRET = os.getenv('STRIPE SECRET')
+STRIPE_SECRET = os.getenv('STRIPE_SECRET')
 
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 
