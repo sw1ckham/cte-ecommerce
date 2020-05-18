9feb263a9d1f2c411894e689a57cb4b7aa2957e8
Sophie Wickham <sphwick@gmail.com>
2020-05-18 16:40:05 +0000

    Added js toggle code for product description and modal js for photo enlargement

---
+++ static/js/product.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('.toggle-description').hide();
+
+    $(".toggle-button").click(function() {
+    $(".toggle-description").toggle("show");
+    });
+
+    var t = document.getElementById("myModal"),
+    c = ($(".myImg"), $("#img01"));
+    $(".myImg").click(function () {
+        t.style.display = "block";
+        var e = this.src;
+        c.attr("src", e);
+    }),
+    (document.getElementsByClassName("close")[0].click = function () {
+        t.style.display = "none";
+    });
+
+
+})
