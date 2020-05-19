9793f7c1321c707345123a7239a61f14db612b94
Sophie Wickham <sphwick@gmail.com>
2020-05-19 15:15:51 +0000

    Added catergorie filter options on views

--- gallery/views.py
+++ gallery/views.py
@@ -4,8 +4,10 @@ from .models import Art
 # Create your views here.
 
 def all_paper(request):
-    return render(request, 'paper.html')
+    art = Art.objects.all().filter(catergorie='paper')
+    return render(request, 'paper.html', {"art": art})
 
 
 def all_canvas(request):
-    return render(request, 'canvas.html')
+    art = Art.objects.all().filter(catergorie='canvas')
+    return render(request, 'canvas.html', {"art": art})
