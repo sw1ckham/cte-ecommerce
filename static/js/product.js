e459fd524361f89ba713a18a768d937fa954b2cf
Sophie Wickham <sphwick@gmail.com>
2020-05-20 10:05:27 +0000

    Edited toggle jquery to toggle only only description at a time

--- static/js/product.js
+++ static/js/product.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $('.toggle-description').hide();
 
     $(".toggle-button").click(function() {
-    $(".toggle-description").toggle("show");
+    $(this).next(".toggle-description").slideToggle("show");
     });
 
     var t = document.getElementById("myModal"),
@@ -15,6 +15,26 @@ $(document).ready(function(){
     (document.getElementsByClassName("close")[0].click = function () {
         t.style.display = "none";
     });
+
+
+    // var onResize = function() {
+// apply dynamic padding at the top of the body according to the fixed navbar height
+    // $("body").css("padding-top", $(".navbar-fixed-top").height());
+    // };
+// attach the function to the window resize event
+    // $(window).resize(onResize);
+// call it also when the page is ready after load or reload
+    // $(function() {
+    //     onResize();
+    // });
+
+    // $(window).resize(function () { 
+    // $('body').css('padding-top', parseInt($('#main-navbar').css("height"))+10);
+    // });
+
+    // $(window).load(function () { 
+    // $('body').css('padding-top', parseInt($('#main-navbar').css("height"))+10);         
+    // }); 
 
 
 })
