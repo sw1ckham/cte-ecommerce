b7bf60c600e1f41f45d561dcc0743e86b05431b0
Sophie Wickham <sphwick@gmail.com>
2020-05-27 14:37:33 +0000

    js to scroll back up the page

--- static/js/scroll.js
+++ static/js/scroll.js
@@ -1,14 +1,16 @@
+// Code taken from https://developpaper.com/django-builds-personal-blogs-back-to-the-top-floating-buttons-vector-icons-footer-sinks-and-sticky-sidebars/
+
 $(document).ready(function(){
     $(function () {
         $('#BackTop').click(function () {
             $('html,body').animate({scrollTop: 0}, 500);
         });
-        $(window).scroll(function () {
-            if ($(this).scrollTop() > 300) {
-                $('#BackTop').fadeIn(300);
-            } else {
-                $('#BackTop').stop().fadeOut(300);
-            }
-        }).scroll();
+        // $(window).scroll(function () {
+        //     if ($(this).scrollTop() > 300) {
+        //         $('#BackTop').fadeIn(300);
+        //     } else {
+        //         $('#BackTop').stop().fadeOut(300);
+        //     }
+        // }).scroll();
     });
 })
