ed25d7aabd452cf3d25328bc40a85641d5b84ff9
Sophie Wickham <sphwick@gmail.com>
2020-05-27 17:48:54 +0000

    Added checkout success url

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import checkout
+from .views import checkout, success
 
 urlpatterns = [
     url(r'^$', checkout, name='checkout'),
+    url(r'^$', success, name="success")
 ]
