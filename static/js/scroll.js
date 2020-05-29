6addd8e7784dbb38ef5b8d310c1336e508b9427f
Sophie Wickham <sphwick@gmail.com>
2020-05-29 15:07:12 +0000

    Experimented with back to top button disappearing

--- static/js/scroll.js
+++ static/js/scroll.js
@@ -1,16 +1,31 @@
 // Code taken from https://developpaper.com/django-builds-personal-blogs-back-to-the-top-floating-buttons-vector-icons-footer-sinks-and-sticky-sidebars/
 
 $(document).ready(function(){
-    $(function () {
-        $('#BackTop').click(function () {
-            $('html,body').animate({scrollTop: 0}, 500);
+   
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 200) {
+                $('#BackTop').css('display', 'block')
+            } else {
+                $('#BackTop').css('display', 'none')
+            }
         });
-        // $(window).scroll(function () {
-        //     if ($(this).scrollTop() > 300) {
-        //         $('#BackTop').fadeIn(300);
-        //     } else {
-        //         $('#BackTop').stop().fadeOut(300);
-        //     }
-        // }).scroll();
-    });
-})
+
+
+        // mybutton = document.getElementById("BackTop");
+
+        // // When the user scrolls down 20px from the top of the document, show the button
+        // window.onscroll = function() {scrollFunction()};
+
+        // function scrollFunction() {
+        // if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        //     mybutton.style.display = "block";
+        // } else {
+        //     mybutton.style.display = "none";
+        // }
+        // }
+            $('#BackTop').click(function () {
+            $('html,body').animate({scrollTop: 0}, 500);
+            return false;
+        });
+
+});
