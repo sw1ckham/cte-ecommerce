ca585d66bbda9d58709fa59f5342d4cbc58a4c9a
Sophie Wickham <sphwick@gmail.com>
2020-05-29 18:21:26 +0000

    Deleted old commented out code

--- static/js/product.js
+++ static/js/product.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
     
-    
     $(".toggle-show-button").click(function() {
         $(this).next(".toggle-description").toggleClass("toggle-description-show");
     });
@@ -8,25 +7,5 @@ $(document).ready(function(){
     $('.toggle-hide-icon').click(function(){
         $(this).next(".toggle-description-show").hide();
     });
-
-    // $('#myModal').on('shown.bs.modal', function () {
-    // $('#myInput').trigger('focus')
-    // });
-
-    // console.log('load')
-    // var t = document.getElementById("myModal"),
-    // c = ($(".myImg"), $("#img01"));
-    // $(".myImg").click(function () {
-    //     console.log(this);
-    //     t.style.display = "block";
-    //     var e = this.src;
-    //     console.log(this.src);
-    //     c.attr("MEDIA_URL", e);
-        
-    // }),
-
-    // (document.getElementsByClassName("close")[0].click = function () {
-    //     t.style.display = "none";
-    // });
 
 });
